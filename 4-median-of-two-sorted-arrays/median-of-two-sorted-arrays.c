b7d23a4a71c07cf7c5ddbc94b75c45431cea062f
Alvin Mun <73019337+AlvinMun@users.noreply.github.com>
2025-03-29 00:06:13 +0800

    Time: 4 ms (27.32%) | Memory: 11.4 MB (47.24%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.c
@@ -0,0 +1,30 @@
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int m = nums1Size;
+    int n = nums2Size;
+    int p1 = 0;
+    int p2 = 0;
+    int min() {
+        if (p1 < m && p2 < n) {
+            return (nums1[p1] < nums2[p2] ? nums1[p1++] : nums2[p2++]);
+        } else if (p1 < m) {
+            return nums1[p1++];
+        } else if (p2 < n) {
+            return nums2[p2++];
+        }
+        return (-1);
+    }
+
+    double median;
+    if ((m + n) % 2 == 0) {
+        for (int i = 0; i < ((m + n) / 2) - 1; ++i) {
+            int temp = min();
+        }
+        median = (min() + min()) / 2.0;
+    } else {
+        for (int i = 0; i < (m + n) / 2; ++i) {
+            int temp = min();
+        }
+        median = min();
+    }
+    return (median);
+}
