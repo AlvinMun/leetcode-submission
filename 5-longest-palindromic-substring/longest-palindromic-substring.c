4f54f59acbacd311d33ef3ae7c70dee169aa4110
Alvin Mun <73019337+AlvinMun@users.noreply.github.com>
2025-03-30 17:05:49 +0800

    Time: 111 ms (47.08%) | Memory: 9.5 MB (12.00%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.c
@@ -0,0 +1,40 @@
+char* longestPalindrome(char* s) {
+    int n = strlen(s);
+    if (n == 0)
+        return "";
+    bool dp[n][n];
+    memset(dp, false, sizeof dp);
+    int result[2] = {0, 0};
+
+    //for single character substring
+    for (int i = 0; i < n; i++) {
+        dp[i][i] = true;
+    }
+
+    //for 2-character substring
+    for (int i = 0; i < n - 1; i++) {
+        if (s[i] == s[i + 1]) {
+            dp[i][i + 1] = true;
+            result[0] = i;
+            result[1] = i + 1;
+        } else {
+            dp[i][i + 1] = false;
+        }
+    }
+
+    //for 3-character substring or more
+    for (int diff = 2; diff < n; diff++) {
+        for (int i = 0; i < n - diff; i++) {
+            int j = i + diff; //since diff = j - i
+            if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                dp[i][j] = true;
+                result[0] = i;
+                result[1] = j;
+            } else {
+                dp[i][j] = false;
+            }
+        }
+    }
+    s[result[1] + 1] = '\0';
+    return s + result[0];
+}
