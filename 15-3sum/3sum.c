c694694f761ca02d2af57c8a31be195fe5e15dbd
Alvin Mun <73019337+AlvinMun@users.noreply.github.com>
2025-04-12 22:44:18 +0800

    Time: 47 ms (74.79%) | Memory: 36.1 MB (84.15%) - LeetSync

---
+++ 15-3sum/3sum.c
@@ -0,0 +1,61 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int** threeSum(int *nums, int numsSize, int *returnSize, int **returnColumnSizes) {
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    int **res = malloc(100 * sizeof(int *));  // Start with 100 elements, adjust later
+    *returnColumnSizes = malloc(100 * sizeof(int));
+    *returnSize = 0;
+    int capacity = 100;  // Initial capacity for results
+
+    for (int i = 0; i < numsSize - 2; i++) {
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+
+        int left = i + 1;
+        int right = numsSize - 1;
+
+        while (left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if (sum < 0) {
+                left++;
+            } else if (sum > 0) {
+                right--;
+            } else {
+                // If we've used all initial allocated space, reallocate
+                if (*returnSize == capacity) {
+                    capacity *= 2;  // Double the capacity to prevent frequent reallocations
+                    res = realloc(res, capacity * sizeof(int *));
+                    *returnColumnSizes = realloc(*returnColumnSizes, capacity * sizeof(int));
+                }
+
+                res[*returnSize] = malloc(3 * sizeof(int));
+                res[*returnSize][0] = nums[i];
+                res[*returnSize][1] = nums[left];
+                res[*returnSize][2] = nums[right];
+                (*returnColumnSizes)[*returnSize] = 3;
+                (*returnSize)++;
+
+                // Skip duplicates
+                while (left < right && nums[left] == nums[left + 1])
+                    left++;
+                while (left < right && nums[right] == nums[right - 1])
+                    right--;
+
+                left++;
+                right--;
+            }
+        }
+    }
+
+    return res;
+}
+
