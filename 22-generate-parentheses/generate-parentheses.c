9ebb41789231abaa6edd9a9105317960fae21cb1
Alvin Mun <73019337+AlvinMun@users.noreply.github.com>
2025-04-20 16:36:29 +0800

    Time: 7 ms (7.46%) | Memory: 25.6 MB (5.22%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.c
+++ 22-generate-parentheses/generate-parentheses.c
@@ -1,36 +1,35 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-void backtrack(char ***res, int *returnSize, int *capacity, char *current, int open, int close, int max) {
-    if (strlen(current) == max * 2) {
-        if (*returnSize >= *capacity) {
-            *capacity *= 2;
-            *res = realloc(*res, (*capacity) * sizeof(char *));
-        }
-        (*res)[*returnSize] = strdup(current);
+void backtrack(char ***res, int *returnSize, char *current, int open, int close, int max) {
+    int len = strlen(current);
+    if (len == 2 * max) {
         (*returnSize)++;
+        *res = realloc(*res, (*returnSize) * sizeof(char*));
+        (*res)[*returnSize - 1] = malloc((2 * max + 1) * sizeof(char));
+        strcpy((*res)[*returnSize - 1], current);
         return;
     }
-
+    
     if (open < max) {
-        current[strlen(current)] = '(';
-        backtrack(res, returnSize, capacity, current, open + 1, close, max);
-        current[strlen(current) - 1] = '\0';
+        current[len] = '(';
+        current[len + 1] = '\0';
+        backtrack(res, returnSize, current, open + 1, close, max);
+        current[len] = '\0';
     }
     if (close < open) {
-        current[strlen(current)] = ')';
-        backtrack(res, returnSize, capacity, current, open, close + 1, max);
-        current[strlen(current) - 1] = '\0';
+        current[len] = ')';
+        current[len + 1] = '\0';
+        backtrack(res, returnSize, current, open, close + 1, max);
+        current[len] = '\0';
     }
 }
 
 char** generateParenthesis(int n, int* returnSize) {
-    int capacity = 100;
-    char **res = malloc(capacity * sizeof(char *));
     *returnSize = 0;
-
-    char *current = calloc(2 * n + 1, sizeof(char));
-    backtrack(&res, returnSize, &capacity, current, 0, 0, n);
+    char **res = NULL;
+    char *current = calloc((2 * n + 1), sizeof(char));
+    backtrack(&res, returnSize, current, 0, 0, n);
     free(current);
     return res;
 }
