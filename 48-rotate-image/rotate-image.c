f063e6694694b5806c85a2337f6f9c75287bf1ed
Alvin Mun <73019337+AlvinMun@users.noreply.github.com>
2025-08-19 17:00:27 +0800

    Time: 0 ms (100.00%) | Memory: 10.2 MB (72.85%) - LeetSync

---
+++ 48-rotate-image/rotate-image.c
@@ -0,0 +1,29 @@
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void transpose(int **matrix, int len) {
+    for (int i = 0; i < len; i++) {
+        for (int j = i; j < len; j++) {
+            swap(&matrix[i][j], &matrix[j][i]);
+        }
+    }
+}
+void changeColumns(int **matrix, int len) {
+    for (int i = 0; i < len; i++) {
+        if (len % 2 == 0) {
+            for (int j = 0; j < len / 2; j++) {
+                swap(&matrix[i][j], &matrix[i][len - j - 1]);
+            }
+        } else {
+            for (int j = 0; j < (len / 2) + 1; j++) {
+                swap(&matrix[i][j], &matrix[i][len - j - 1]);
+            }
+        }
+    }
+}
+void rotate(int **matrix, int matrixSize, int *matrixColSize) {
+    transpose(matrix, matrixSize);
+    changeColumns(matrix, matrixSize);
+}
